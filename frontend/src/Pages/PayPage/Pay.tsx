6bce12c09a8d3d0534dc795d36f6ca2f72354c64
fpmovec <10.maks.10K@gmail.com>
2023-07-31 13:49:55 +0300

    paypal

--- frontend/src/Pages/PayPage/Pay.tsx
+++ frontend/src/Pages/PayPage/Pay.tsx
@@ -1,6 +1,50 @@
-import { useParams } from "react-router-dom"
+import { useParams } from "react-router-dom";
+import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
+import { useAppSelector } from "../../Redux/Hooks"; 
+import styles from "./Pay.module.css";
+import { bookedConcert } from "../../Redux/Slices";
+
+const initialOptions = {
+  clientId:
+    "AWNjTVENcpaNZkUd75U1rv8HTKM3mLRmjIDMOj3fDGVrC3N8LBo0AE5cBIc6iv1CHbmnoIZwGeWyGeru",
+  currency: "USD",
+  intent: "capture",
+};
 
 export const Pay = () => {
-    const { bookingId } = useParams();
-    return <h2>Booking id for pay: {bookingId}</h2>
-}
+  const { bookingId } = useParams();
+
+  const allBookings = useAppSelector(state => state.concerts.booking);
+  console.log(allBookings);
+  const currentBooking = [...allBookings].filter(b => b.id === Number(bookingId))[0];
+  console.log(currentBooking);
+  return (
+    <div className={styles.main}>
+      <div className={styles.buttons}>
+        <PayPalScriptProvider options={initialOptions}>
+          <PayPalButtons
+            createOrder={(data, actions) => {
+              return actions.order.create({
+                purchase_units: [
+                  {
+                    amount: {
+                      value: currentBooking.purchaseAmount.toString(),
+                    },
+                  },
+                ],
+              });
+            }}
+            onApprove={(data, actions) => {
+              return actions.order!.capture().then(function (details) {
+                console.log(
+                  "Transaction completed by " + 
+                  details.payer.name!.given_name!
+                );
+              });
+            }}
+          />
+        </PayPalScriptProvider>
+      </div>
+    </div>
+  );
+};
