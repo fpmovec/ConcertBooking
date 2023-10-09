ca6d235e59b9baa86f5d8f99076b39f77605943a
fpmovec <10.maks.10K@gmail.com>
2023-10-09 22:37:30 +0300

    docker-compose16

--- frontend/src/Requests/GET/ConcertsRequests.ts
+++ frontend/src/Requests/GET/ConcertsRequests.ts
@@ -10,7 +10,7 @@ import {
 export const GetAllConcerts = async (): Promise<Concert[]> => {
   let allConcerts: Concert[] = [];
 
-  const response = await fetch("https://localhost:80/concerts", {
+  const response = await fetch("https://localhost:7235/concerts", {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
